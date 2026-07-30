a54639339896de32acc07b96e6c570fec8b96567
Sounyatje <sounya-abattouy@live.be>
2026-07-30 16:22:10 +0200

    chooseside function

--- src/player/AIPlayer.java
+++ src/player/AIPlayer.java
@@ -4,4 +4,12 @@ public class AIPlayer extends Participant {
     public AIPlayer(String name) {
         super(name, 0);
     }
+    public String chooseSide() {
+        double random = Math.random();
+        if (random < 0.5) {
+            return "HEADS";
+        } else {
+            return "TAILS";
+        }
+    }
 }
